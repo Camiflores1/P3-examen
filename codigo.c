c64bd719d48403a3c8b15154d06261ae447596fa
Camiflores1 <monse.flores0108@gmail.com>
2023-07-11 12:59:19 -0500

    Cambios para que se imprima en el texto nuevo

--- codigo.c
+++ codigo.c
@@ -2,41 +2,41 @@
 #include <string.h>
 #include <ctype.h>
 
-void corregirTexto(const char* tricolor, const char* archivoSalida); // Declaración de la función 
+void corregirTexto(const char* archivoI, const char* archivoS); // Declaración de la función 
 
 int main() {
-    const char* archivoEntrada = "tricolor.txt"; 
-    const char* archivoSalida = "tricolor.txt"; 
-    corregirTexto(archivoEntrada, archivoSalida);
+    const char* archivoS = "tri.txt"; 
+    const char* archivoI = "tri.txt"; 
+    corregirTexto(archivoI, archivoS);
     return 0;
 }
 
-void corregirTexto(const char* archivoEntrada, const char* archivoSalida) {
-    FILE* archivoEntradaPtr = fopen(tricolor.txt, "r"); // Abrir el archivo de entrada en modo lectura
-    if (archivoEntradaPtr == NULL) { // Comprobar si hubo un error al abrir el archivo de entrada
-        printf("Corrección.\n");
+void corregirTexto(const char* archivoI, const char* archivoS) {
+    FILE* archivoIPtr = fopen("tri.txt", "r"); // Abrir el archivo de entrada en modo lectura
+    if (archivoIPtr == NULL) { // Identifica si hubo un error al abrir el archivo de entrada
+        printf("corregirTexto.\n");
         return;
     }
 
-    FILE* archivoSalidaPtr = fopen(archivoSalida, "w"); // Abrir el archivo en modo escritura
-    if (archivoSalidaPtr == NULL) { // Comprobar si hubo un error al abrir el archivo de salida
-        printf("Corrección".\n");
-        fclose(archivoEntradaPtr);
+    FILE* archivoSPtr = fopen(archivoS, "w"); // Abrir el archivo en modo escritura
+    if (archivoSPtr == NULL) { // Nos ayuda a comprobar si hubo un error al abrir el archivo de salida
+        printf("corregirTexto".\n");
+        fclose(archivoIPtr);
         return;
     }
 
-    char palabra[50]; 
+    char parrafo[50]; 
 
-    while (fscanf(archivoEntradaPtr, "%s", palabra) == 1) { // Leer palabras del archivo de entrada hasta el final
-        if (strncmp(palabra, "ecuador", 7) == 0 && islower(palabra[0])) { // Comprobar si la palabra es "ecuador" y la primera letra es minúscula
-            palabra[0] = toupper(palabra[0]); // Cambiar la primera letra a mayúscula
+    while (fscanf(archivoIPtr, "%s", parrafo) == 1) { // Leer palabras del archivo de entrada hasta el final
+        if (strncmp(parrafo, "ecuador", 7) == 0 && islower(palabra[0])) { // Comprobar si la palabra es "Ecuador" y la primera letra debe ir con mayúscula
+            parrafo[0] = toupper(parrafo[0]); // Se cambiará la primera letra a mayúscula
         }
 
-        fprintf(archivoSalidaPtr, "%s ", palabra); 
+        fprintf(archivoSPtr, "%s ", parrafo); 
     }
 
-    fclose(archivoEntradaPtr); 
-    fclose(archivoSalidaPtr); 
+    fclose(archivoIPtr); 
+    fclose(archivoSPtr); 
 
     printf("Archivo corregido.\n"); 
 }
